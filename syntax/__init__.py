73601eaa8c066ee53abecfc8764f0af682c0ac78
Chris Bradfield <chris@kidscancode.org>
2020-09-06 17:35:07 -0700

    add checks

--- syntax/__init__.py
+++ syntax/__init__.py
@@ -1,14 +1,13 @@
-import re
-
 import check50
 import check50.c
 
 @check50.check()
 def exists():
-    """syntax.c exists"""
+    """syntax.c exists."""
     check50.exists("syntax.c")
 
-@check50.check(compiles)
+
+@check50.check(exists)
 def compiles():
-    """"syntax.c compiles"""
+    """syntax.c compiles."""
     check50.c.compile("syntax.c", lcs50=True)
