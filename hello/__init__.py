0463174dfe17883ce3f9918bd2c085724d19dc46
Chris Bradfield <chris@kidscancode.org>
2020-09-16 18:01:38 -0700

    updates

--- hello/__init__.py
+++ hello/__init__.py
@@ -1,22 +1,16 @@
 import check50
-import check50.c
 
 @check50.check()
 def exists():
-    """hello.c exists."""
-    check50.exists("hello.c")
+    """hello.py exists."""
+    check50.exists("hello.py")
 
 @check50.check(exists)
-def compiles():
-    """hello.c compiles."""
-    check50.c.compile("hello.c", lcs50=True)
-
-@check50.check(compiles)
 def veronica():
     """responds to name Veronica."""
-    check50.run("./hello").stdin("Veronica").stdout("Veronica").exit()
+    check50.run("python hello.py").stdin("Veronica").stdout("Veronica").exit()
 
 @check50.check(compiles)
 def brian():
     """responds to name Brian."""
-    check50.run("./hello").stdin("Brian").stdout("Brian").exit()
+    check50.run("python hello.py").stdin("Brian").stdout("Brian").exit()
