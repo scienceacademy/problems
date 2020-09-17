16ccf03af0d1f5abf1f139f5c95c25d5498048d6
Chris Bradfield <chris@kidscancode.org>
2020-09-17 08:59:53 -0700

    updates

--- hello/__init__.py
+++ hello/__init__.py
@@ -5,10 +5,10 @@ def exists():
     """hello.py exists."""
     check50.exists("hello.py")
 
-# @check50.check(exists)
-# def veronica():
-#     """responds to name Veronica."""
-#     check50.run("python3 hello.py").stdin("Veronica").stdout("Veronica").exit(0)
+@check50.check(exists)
+def veronica():
+    """responds to name Veronica."""
+    check50.run("python3 hello.py").stdin("Veronica").stdout("Veronica").exit(0)
 
 # @check50.check(compiles)
 # def brian():
