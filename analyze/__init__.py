1ddb822fdd8944eececf25f6c255bb8a11a41dd7
Chris Bradfield <chris@kidscancode.org>
2021-01-27 14:02:18 -0800

    updates

--- analyze/__init__.py
+++
@@ -1,8 +0,0 @@
-import check50
-
-# Coming soon...
-
-@check50.check()
-def submitted():
-    """Defender of the Web submitted"""
-    pass
--- tech/__init__.py
+++
@@ -1,8 +0,0 @@
-import check50
-
-# Coming soon...
-
-@check50.check()
-def submitted():
-    """Tech Spotlight submitted"""
-    pass
