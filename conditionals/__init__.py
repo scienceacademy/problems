c65eafda8d0fc9c43ed59f0e00b6ebaedb9a3991
Chris Bradfield <chris@kidscancode.org>
2021-01-27 08:26:55 -0800

    updates

--- conditionals/__init__.py
+++ conditionals/__init__.py
@@ -9,35 +9,41 @@ def exists():
 
 @check50.check(exists)
 def testA():
-    """responds to input of 95."""
+    """grades.py responds to input of 95."""
     check50.run("python3 grades.py").stdin("95").stdout("You got an A!").exit(0)
 
 
 @check50.check(exists)
 def testB():
-    """responds to input of 85."""
+    """grades.py responds to input of 85."""
     check50.run("python3 grades.py").stdin("85").stdout("You got a B!").exit(0)
 
 
 @check50.check(exists)
 def testC():
-    """responds to input of 75."""
+    """grades.py responds to input of 75."""
     check50.run("python3 grades.py").stdin("75").stdout("You got a C!").exit(0)
 
 
 @check50.check(exists)
 def testA():
-    """responds to input of 65."""
+    """grades.py responds to input of 65."""
     check50.run("python3 grades.py").stdin("65").stdout("You got a D!").exit(0)
 
 
 @check50.check(exists)
 def test_even():
-    """responds to input of 8."""
+    """odd_even.py responds to input of 8."""
     check50.run("python3 odd_even.py").stdin("8").stdout("Even").exit(0)
 
 
 @check50.check(exists)
 def test_odd():
-    """responds to input of 11."""
+    """odd_even.py responds to input of 11."""
     check50.run("python3 odd_even.py").stdin("11").stdout("Odd").exit(0)
+
+
+@check50.check(exists)
+def test_zero():
+    """odd_even.py responds to input of 0."""
+    check50.run("python3 odd_even.py").stdin("0").stdout("Even").exit(0)
