cb5eea0672222c09062bc6ba6c4e6c1817026936
Chris Bradfield <chris@scribe.net>
2021-09-28 20:56:47 -0700

    updates

--- forloop/__init__.py
+++ forloop/__init__.py
@@ -10,16 +10,16 @@ def exists():
 @check50.check(exists)
 def result():
     """result is 55"""
-    check50.run("python3 forloop.py").stdout("55").exit(0)
+    check50.run("python3 forloop.py").stdout("110").exit(0)
 
 
 @check50.check(exists)
 def testB():
-    """string.py outputs correctly with 'aaron'."""
+    """vowels.py outputs correctly with 'aaron'."""
     check50.run("python3 vowels.py").stdin("aaron").stdout("3").exit(0)
 
 
 @check50.check(exists)
 def testC():
-    """string.py outputs correctly with 'alexandria'."""
+    """vowels.py outputs correctly with 'alexandria'."""
     check50.run("python3 vowels.py").stdin("alexandria").stdout("5").exit(0)
