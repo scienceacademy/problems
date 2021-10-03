baccbeb317df1f2d02f7a8bb131940f878f8f8bd
Chris Bradfield <chris@scribe.net>
2021-10-03 16:35:56 -0700

    updates

--- lists/__init__.py
+++ lists/__init__.py
@@ -2,24 +2,19 @@ import check50
 
 @check50.check()
 def exists():
-    """list.py and vowels.py exist."""
-    check50.exists("list.py")
-    check50.exists("vowels.py")
+    """smallest.py and odds.py exist."""
+    check50.exists("smallest.py")
+    check50.exists("odds.py")
 
 
 @check50.check(exists)
 def testA():
-    """list.py outputs the correct squared values."""
-    check50.run("python3 list.py").stdout("[4, 49, 16, 25, 81]").exit(0)
+    """smallest.py outputs the correct smallest value."""
+    check50.run("python3 smallest.py").stdout("4").exit(0)
 
 
 @check50.check(exists)
 def testB():
-    """string.py outputs correctly with 'aaron'."""
-    check50.run("python3 vowels.py").stdin("aaron").stdout("3").exit(0)
+    """odds.py outputs correctly."""
+    check50.run("python3 odds.py").stdout("4").exit(0)
 
-
-@check50.check(exists)
-def testC():
-    """string.py outputs correctly with 'alexandria'."""
-    check50.run("python3 vowels.py").stdin("alexandria").stdout("5").exit(0)
