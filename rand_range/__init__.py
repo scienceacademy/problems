63c66faf252af61a1dca84e49dfe43f5c4842d0e
Chris Bradfield <chris@scribe.net>
2021-10-06 08:11:32 -0700

    updates

--- rand_range/__init__.py
+++ rand_range/__init__.py
@@ -13,16 +13,16 @@ def compiles():
 
 @check50.check(compiles)
 def test1():
-    """1-6"""
+    """1-6 => 6"""
     check50.run("./rand_range 1 6").stdout("6\n").exit()
 
 @check50.check(compiles)
 def test2():
-    """15-25"""
+    """15-25 => 22"""
     check50.run("./rand_range 15 25").stdout("22\n").exit()
 
 @check50.check(compiles)
 def test3():
-    """938-2843"""
+    """938-2843 => 2841"""
     check50.run("./rand_range 938 2843").stdout("2841\n").exit()
 
