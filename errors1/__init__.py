dfe5cfe866c8b8e16ac51d5e732e571c1a0bf3ce
Chris Bradfield <chris@scribe.net>
2021-10-17 20:09:53 -0700

    updates

--- errors1/__init__.py
+++ errors1/__init__.py
@@ -3,44 +3,44 @@ import check50
 
 @check50.check()
 def exists():
-    """p1-4.py exist."""
-    check50.exists("p1.py")
-    check50.exists("p2.py")
-    check50.exists("p3.py")
-    check50.exists("p4.py")
+    """error1-4.py exist."""
+    check50.exists("error1.py")
+    check50.exists("error2.py")
+    check50.exists("error3.py")
+    check50.exists("error4.py")
 
 
 @check50.check(exists)
 def test_p1():
-    """p1.py produces the correct output."""
-    check50.run("python3 p1.py").stdin("Wanda").stdout("Wanda").exit(0)
+    """error1.py produces the correct output."""
+    check50.run("python3 error1.py").stdin("Wanda").stdout("Wanda").exit(0)
 
 
 @check50.check(exists)
 def test_p2():
-    """p2.py outputs correctly with '10'."""
-    check50.run("python3 p2.py").stdin("10").stdout("20").exit(0)
+    """error2.py outputs correctly with '10'."""
+    check50.run("python3 error2.py").stdin("10").stdout("20").exit(0)
 
 
 @check50.check(exists)
 def test_p3a():
-    """p3.py outputs correctly with '1'."""
-    check50.run("python3 p3.py").stdin("1").stdout("1").exit(0)
+    """error3.py outputs correctly with '1'."""
+    check50.run("python3 error3.py").stdin("1").stdout("1").exit(0)
 
 
 @check50.check(exists)
 def test_p3b():
-    """p3.py outputs correctly with '2'."""
-    check50.run("python3 p3.py").stdin("2").stdout("2").exit(0)
+    """error3.py outputs correctly with '2'."""
+    check50.run("python3 error3.py").stdin("2").stdout("2").exit(0)
 
 
 @check50.check(exists)
 def test_p3c():
-    """p3.py outputs correctly with '3'."""
-    check50.run("python3 p3.py").stdin("3").stdout("3").exit(0)
+    """error3.py outputs correctly with '3'."""
+    check50.run("python3 error3.py").stdin("3").stdout("3").exit(0)
 
 
 @check50.check(exists)
 def test_p4():
-    """p4.py outputs correctly with '1, 2, 3, 4, 5'."""
-    check50.run("python3 p4.py").stdin("1").stdin("2").stdin("3").stdin("4").stdin("5").stdout("120").exit(0)
+    """error4.py outputs correctly with '1, 2, 3, 4, 5'."""
+    check50.run("python3 error4.py").stdin("1").stdin("2").stdin("3").stdin("4").stdin("5").stdout("120").exit(0)
