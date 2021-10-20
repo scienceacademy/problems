884b6b5d930748636afe9f0dc13da18dd5612b80
Chris Bradfield <chris@scribe.net>
2021-10-19 19:04:10 -0700

    updates

---
+++ concat/__init__.py
@@ -0,0 +1,17 @@
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+    """Files exist."""
+    check50.exists(concat.c")
+
+@check50.check(exists)
+def compiles():
+    """Programs compile."""
+    check50.c.compile("concat.c", lcs50=True)
+
+@check50.check(compiles)
+def add1():
+    """adds 'hello' and 'world' """
+    check50.run("./concat").stdin("hello").stdin("world").stdout("helloworld\n").exit()
