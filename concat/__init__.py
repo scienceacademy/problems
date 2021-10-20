f5174743ddca94c7bee4f9dd4fa1732d07b0aaa0
Chris Bradfield <chris@scribe.net>
2021-10-19 19:10:14 -0700

    updates

--- concat/__init__.py
+++ concat/__init__.py
@@ -15,3 +15,10 @@ def compiles():
 def add1():
     """adds 'hello' and 'world' """
     check50.run("./concat").stdin("hello").stdin("world").stdout("helloworld\n").exit()
+
+@check50.check(exists)
+def frees():
+    """Uses free() to free memory"""
+    app = open("concat.c").read()
+    if 'free(' not in app:
+        raise check50.Failure(f"You don't seem to be freeing allocated memory!")
