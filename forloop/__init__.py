5c3bd7188d92e553530392d6dcd9684c53a5341b
Chris Bradfield <cb@scribe.net>
2023-01-30 15:06:57 -0800

    Update __init__.py

--- forloop/__init__.py
+++ forloop/__init__.py
@@ -2,24 +2,11 @@ import check50
 
 @check50.check()
 def exists():
-    """forloop.py and vowels.py exist."""
+    """forloop.py exists."""
     check50.exists("forloop.py")
-    check50.exists("vowels.py")
 
 
 @check50.check(exists)
 def result():
-    """result is 110"""
-    check50.run("python3 forloop.py").stdout("110").exit(0)
-
-
-@check50.check(exists)
-def testB():
-    """vowels.py outputs correctly with 'aaron'."""
-    check50.run("python3 vowels.py").stdin("aaron").stdout("3").exit(0)
-
-
-@check50.check(exists)
-def testC():
-    """vowels.py outputs correctly with 'alexandria'."""
-    check50.run("python3 vowels.py").stdin("alexandria").stdout("5").exit(0)
+    """result is 55"""
+    check50.run("python3 forloop.py").stdout("55").exit(0)
