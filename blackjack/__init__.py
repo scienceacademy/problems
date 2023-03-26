988d8913c4f170d0658cf92440983a0bd3f05855
Chris Bradfield <cb@scribe.net>
2023-03-26 11:35:19 -0700

    updates

--- blackjack/__init__.py
+++ blackjack/__init__.py
@@ -21,7 +21,7 @@ def hand3():
     check50.run("python blackjack_score.py 4C 5H").stdout("9")
 
 @check50.check(exists)
-def hand4()):
+def hand4():
     """2 cards with one ace, total > 21 if ace is 11"""
     check50.run("python blackjack_score.py 10S AH").stdout("11")
 
@@ -41,7 +41,7 @@ def hand7():
     check50.run("python blackjack_score.py AH 5C 2S").stdout("18")
 
 @check50.check(exists)
-def hand8()):
+def hand8():
     """4 cards with one ace, reduced to 1"""
     check50.run("python blackjack_score.py AS 8D 4C 6D").stdout("19")
 
