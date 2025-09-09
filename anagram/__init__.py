5336eab1e88965e5c1a0b565bfcad3b596df0eb0
Chris Bradfield <cb@scribe.net>
2025-09-08 19:20:11 -0700

    updates

--- anagram/__init__.py
+++ anagram/__init__.py
@@ -34,5 +34,5 @@ def test2():
 
 @check50.check(compiles)
 def test3():
-    """Check for not non alphabetoc"""
-    check50.run("./anagram").stdin("apple").stdin("color8").stdout("Alphabetic characters only.").exit(0)
+    """Check for not non alphabetic"""
+    check50.run("./anagram").stdin("apple").stdin("color8").stdout("Alphabetic characters only.").exit(1)
