2cb87a09662c4f39dc6c7a789b9e219ebe282ce3
Chris Bradfield <cb@scribe.net>
2025-10-02 16:11:13 -0700

    updates

--- filter_t/more/testing.c
+++ filter_t/more/testing.c
@@ -298,6 +298,8 @@ int main(int argc, char *argv[])
         }
         case 1:
         {
+            pixelate(3, 3, img4);
+            print_pixel(img4[0][1]);
             break;
         }
         case 2:
