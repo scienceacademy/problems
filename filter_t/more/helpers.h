6d33e90915da60ef5741088b429913ed3ab3cd22
Chris Bradfield <cb@scribe.net>
2025-10-09 13:26:51 -0700

    updates

--- filter_t/more/helpers.h
+++ filter_t/more/helpers.h
@@ -4,13 +4,13 @@
 void grayscale(int height, int width, RGBTRIPLE image[height][width]);
 
 // Reflect image horizontally
-void reflect(int height, int width, RGBTRIPLE image[height][width]);
+// void reflect(int height, int width, RGBTRIPLE image[height][width]);
 
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width]);
 
 // Blur image
-void blur(int height, int width, RGBTRIPLE image[height][width]);
+// void blur(int height, int width, RGBTRIPLE image[height][width]);
 
 // Pixelate image
 void pixelate(int height, int width, RGBTRIPLE image[height][width]);
